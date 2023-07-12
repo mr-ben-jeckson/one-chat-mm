cab714830c195df08dbc430e6eb3fea2b7f4c107
Ben Jeckson <thanhtikexaw@gmail.com>
2023-07-13 00:09:56 +0630

    #Auth & #Component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,7 +3,7 @@
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import Input from "../../components/Inputs/Input";
-import Button from "../../components/Inputs/Button";
+import Button from "../../components/Button";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -82,24 +82,55 @@ const AuthForm = () => {
                         />
                     )
                     }
-                        <Input
-                            id="email"
-                            label="Email Address"
-                            type="Email"
-                            register={register}
-                            errors={errors}
-                        />
-                        <Input
-                            id="password"
-                            label="Password"
-                            type="password"
-                            register={register}
-                            errors={errors}
-                        />
-                        <div>
-                            <Button />
-                        </div>
+                    <Input
+                        id="email"
+                        label="Email Address"
+                        type="Email"
+                        register={register}
+                        errors={errors}
+                    />
+                    <Input
+                        id="password"
+                        label="Password"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+
+                        >
+                            {variant == 'LOGIN' ? 'Login' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+                <div className="mt-3">
+                    <div className="relative">
+                        <div
+                            className="
+                                absolute
+                                inset-0
+                                flex
+                                items-center
+                            "
+                        >
+                            <div
+                                className="
+                                    w-full 
+                                    border-t 
+                                    border-gray-300"
+                                 />
+                            <div className="relative flex justify-center text-sm">
+                                <span className="bg-white px-2 text-gray-500">
+                                    
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
